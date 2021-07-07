aafae8b042ca26a67de4c76a4a53c1814aa9520f
Faj5097 <fab.jan@t-online.de>
2021-07-07 11:54:52 +0200

    Data and Images for three different types

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -4,55 +4,85 @@ export const AVOCADOS = [
   new Avocado(
     "A1",
     "Edeka",
-    "Organic Hass",
-    64,
-    4.3,
+    "Hass Class 1",
+    69,
+    3.9,
     0,
     "Mexico",
-    ["High water consumption", "Impact on geology", "Cartel involvement"],
+    [
+      "High water consumption in dry country",
+      "Water extraction impacts geology",
+      "Criminality around Avocado industry"
+    ],
     "A1_Origin",
     "scoreOrange",
     423.2,
     [
       "Long distance transport of 9.841km",
-      "Cooling and Ripening facility needed"
+      "Cooling and Ripening facility requires energy"
     ],
     "A1_Emission",
-    "scoreRed",
-    "Organic & Fair",
-    [
-      "Organic farming without pesdicides",
-      "Fair pay to farmers",
-      "Seasonal purchase"
-    ],
+    "scoreOrange",
+    "Industrial",
+    ["Not organic, use of pesticides", "No fair pay information"],
     "A1_Farming",
-    "scoreGreen"
+    "scoreOrange"
   ),
   new Avocado(
     "A2",
-    "Penny",
-    "Avo 123",
-    53,
-    2.5,
+    "Edeka",
+    "Hass Class 2",
+    43,
+    2.7,
     0,
-    "Venezuela",
-    ["High water consumption", "Impact on geology", "Cartel involvement"],
-    "A1_Origin",
+    "Peru",
+    [
+      "High water consumption in dry country",
+      "Privatized water supply causes drinking water scarcity"
+    ],
+    "A2_Origin",
     "scoreOrange",
     566.2,
     [
-      "Long distance transport of 9.841km",
-      "Cooling and Ripening facility needed"
+      "Long Distance transport of 11.143km",
+      "Cooling and Ripening facility requires energy"
     ],
-    "A1_Emission",
+    "A2_Emission",
     "scoreOrange",
-    "Organic & Fair",
+    "Industrial",
+    ["Not organic, use of pesiticides", "Reports of worker exploitation"],
+    "A2_Farming",
+    "scoreRed"
+  ),
+  new Avocado(
+    "A3",
+    "VollCorner",
+    "Organic Hass",
+    82,
+    4.5,
+    0,
+    "Spain",
     [
-      "Organic farming without pesdicides",
+      "High water consumption w/ sufficient water supply",
+      "Seasonal production",
+      "Investments in sustainable production"
+    ],
+    "A3_Origin",
+    "scoreGreen",
+    122,
+    [
+      "Short transportation of ca. 600km",
+      "Minimal requirement of ripening needed"
+    ],
+    "A3_Emission",
+    "scoreOrange",
+    "Organic and Fair",
+    [
+      "Organic farming without pesticides",
       "Fair pay to farmers",
       "Seasonal purchase"
     ],
-    "A1_Farming",
-    "scoreOrange"
+    "A3_Farming",
+    "scoreGreen"
   )
 ];
